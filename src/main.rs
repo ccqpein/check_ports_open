f19a992c48af6aaec398946e8da68fa4f8c8a3e8
ccQpein <ccqpein@protonmail.com>
2018-03-29 15:10:15 +0800

    add draft code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(mut stream: TcpStream) {
+    let a = b"shoudao\n";
+    let rec = stream.write(a).unwrap();
+    let _ = stream.read(&mut [0; 128]);
+    println!("{:?}", rec);
+    println!("hello?");
+}
+
+fn connect_to_host() {
+    let mut stream = TcpStream::connect("127.0.0.1:9527").unwrap();
+    let a = b"fasong\n";
+    let _ = stream.write(a);
+    let _ = stream.read(&mut [0; 128]);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:9527").unwrap();
+    let mut stream = TcpStream::connect("127.0.0.1:9527").unwrap();
+    let a = b"fasong\n";
+    let _ = stream.write(a);
+    let _ = stream.read(&mut [0; 128]);
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            }
+            Err(_) => println!("error"),
+        }
+    }
 }
