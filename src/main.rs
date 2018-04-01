4fcf02f0cfc62497411d1f762b511761355a247d
ccQpein <ccqpein@protonmail.com>
2018-04-01 08:19:51 +0800

    a bit

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,21 @@
-use std::io::prelude::*;
-use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::net::{SocketAddr, TcpStream};
 
-fn handle_client(mut stream: TcpStream) {
-    let a = b"shoudao\n";
-    let rec = stream.write(a).unwrap();
-    let _ = stream.read(&mut [0; 128]);
-    println!("{:?}", rec);
-    println!("hello?");
-}
+fn read_file() {}
 
-fn connect_to_host(addr: &SocketAddr) {
-    if let Ok(stream) = TcpStream::connect(addr) {
+fn parse_file() {}
+
+fn check_connect_to_host(addr: &SocketAddr) -> bool {
+    if let Ok(_) = TcpStream::connect(addr) {
         println!("Connected to the server!");
-        println!("{:?}", stream);
+        true
     } else {
-        println!("wrong")
+        println!("wrong");
+        false
     }
 }
 
 fn main() {
     let test_socket = SocketAddr::from(([127, 0, 0, 1], 8080));
 
-    connect_to_host(&test_Socket);
-    /*
-    let listener = TcpListener::bind("127.0.0.1:9527").unwrap();
-    let mut stream = TcpStream::connect("127.0.0.1:9527").unwrap();
-    let a = b"fasong\n";
-    let _ = stream.write(a);
-    let _ = stream.read(&mut [0; 128]);
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                handle_client(stream);
-            }
-            Err(_) => println!("error"),
-        }
-    }*/
+    check_connect_to_host(&test_socket);
 }
