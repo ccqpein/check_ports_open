e8e4c69830f4005e6dd99d74b01baccbb413d31c
ccQpein <ccqpein@protonmail.com>
2018-04-03 06:42:07 +0800

    a bit

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+use std::fs::File;
+use std::io::prelude::*;
 use std::net::{SocketAddr, TcpStream};
 
-fn read_file() {}
+fn read_file(filename: &str) -> String {
+    let mut f = File::open(filename).expect("cannot find file");
 
-fn parse_file() {}
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    contents
+}
+
+fn parse_file(filename: &str) {}
 
 fn check_connect_to_host(addr: &SocketAddr) -> bool {
     if let Ok(_) = TcpStream::connect(addr) {
@@ -18,4 +28,6 @@ fn main() {
     let test_socket = SocketAddr::from(([127, 0, 0, 1], 8080));
 
     check_connect_to_host(&test_socket);
+
+    println!("{}", read_file("LICENSE"))
 }
